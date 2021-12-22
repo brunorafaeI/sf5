4eec3cd111494d87c6c9b3632016346fdcf2f3d6
Bruno Rafael <brunorafaelap@gmail.com>
2021-12-22 20:46:40 +0100

    Add unit test

---
+++ backend/tests/DemoTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class DemoTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
